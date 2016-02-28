485b1559a0f534c2eae839a10fb25f1c1e9d33e4
Tino <TinoSiegmund@gmx.de>
2016-02-28 15:51:52 +0100

    + fixed api call since bug + minor changes so that synching works relative ok + removed deletion of db on new start of app + moved trigger insertion to onUpgrade method + included logging for okhttp

--- src/main/java/org/noorganization/instalist/provider/DBOpenHelper.java
+++ src/main/java/org/noorganization/instalist/provider/DBOpenHelper.java
@@ -39,53 +39,6 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         _db.execSQL(TaggedProduct.DATABASE_CREATE);
         _db.execSQL(Ingredient.DATABASE_CREATE);
         _db.execSQL(Recipe.DATABASE_CREATE);
-        _db.execSQL(LogInfo.DB_CREATE_V1);
-
-        // create trigger category
-        _db.execSQL(LogInfo.createTriggerInsertion(Category.TABLE_NAME, Category.COLUMN.ID, eModelType.CATEGORY));
-        _db.execSQL(LogInfo.createTriggerDeletion(Category.TABLE_NAME, Category.COLUMN.ID, eModelType.CATEGORY));
-        _db.execSQL(LogInfo.createTriggerUpdate(Category.TABLE_NAME, Category.COLUMN.ID, eModelType.CATEGORY));
-
-        // create trigger list
-        _db.execSQL(LogInfo.createTriggerInsertion(ShoppingList.TABLE_NAME, ShoppingList.COLUMN.ID, eModelType.LIST));
-        _db.execSQL(LogInfo.createTriggerDeletion(ShoppingList.TABLE_NAME, ShoppingList.COLUMN.ID, eModelType.LIST));
-        _db.execSQL(LogInfo.createTriggerUpdate(ShoppingList.TABLE_NAME, ShoppingList.COLUMN.ID, eModelType.LIST));
-
-        // create trigger product
-        _db.execSQL(LogInfo.createTriggerInsertion(Product.TABLE_NAME, Product.COLUMN.ID, eModelType.PRODUCT));
-        _db.execSQL(LogInfo.createTriggerDeletion(Product.TABLE_NAME, Product.COLUMN.ID, eModelType.PRODUCT));
-        _db.execSQL(LogInfo.createTriggerUpdate(Product.TABLE_NAME, Product.COLUMN.ID, eModelType.PRODUCT));
-
-        // create trigger taggedproduct
-        _db.execSQL(LogInfo.createTriggerInsertion(TaggedProduct.TABLE_NAME, TaggedProduct.COLUMN.ID, eModelType.TAGGED_PRODUCT));
-        _db.execSQL(LogInfo.createTriggerDeletion(TaggedProduct.TABLE_NAME, TaggedProduct.COLUMN.ID, eModelType.TAGGED_PRODUCT));
-        _db.execSQL(LogInfo.createTriggerUpdate(TaggedProduct.TABLE_NAME, TaggedProduct.COLUMN.ID, eModelType.TAGGED_PRODUCT));
-
-        // create trigger list entry
-        _db.execSQL(LogInfo.createTriggerInsertion(ListEntry.TABLE_NAME, ListEntry.COLUMN.ID, eModelType.LIST_ENTRY));
-        _db.execSQL(LogInfo.createTriggerDeletion(ListEntry.TABLE_NAME, ListEntry.COLUMN.ID, eModelType.LIST_ENTRY));
-        _db.execSQL(LogInfo.createTriggerUpdate(ListEntry.TABLE_NAME, ListEntry.COLUMN.ID, eModelType.LIST_ENTRY));
-
-        // create trigger ingredient
-        _db.execSQL(LogInfo.createTriggerInsertion(Ingredient.TABLE_NAME, Ingredient.COLUMN.ID, eModelType.INGREDIENT));
-        _db.execSQL(LogInfo.createTriggerDeletion(Ingredient.TABLE_NAME, Ingredient.COLUMN.ID, eModelType.INGREDIENT));
-        _db.execSQL(LogInfo.createTriggerUpdate(Ingredient.TABLE_NAME, Ingredient.COLUMN.ID, eModelType.INGREDIENT));
-
-        // create trigger recipe
-        _db.execSQL(LogInfo.createTriggerInsertion(Recipe.TABLE_NAME, Recipe.COLUMN.ID, eModelType.RECIPE));
-        _db.execSQL(LogInfo.createTriggerDeletion(Recipe.TABLE_NAME, Recipe.COLUMN.ID, eModelType.RECIPE));
-        _db.execSQL(LogInfo.createTriggerUpdate(Recipe.TABLE_NAME, Recipe.COLUMN.ID, eModelType.RECIPE));
-
-        // create trigger unit
-        _db.execSQL(LogInfo.createTriggerInsertion(Unit.TABLE_NAME, Unit.COLUMN.ID, eModelType.UNIT));
-        _db.execSQL(LogInfo.createTriggerDeletion(Unit.TABLE_NAME, Unit.COLUMN.ID, eModelType.UNIT));
-        _db.execSQL(LogInfo.createTriggerUpdate(Unit.TABLE_NAME, Unit.COLUMN.ID, eModelType.UNIT));
-
-        // create trigger unit
-        _db.execSQL(LogInfo.createTriggerInsertion(Tag.TABLE_NAME, Tag.COLUMN.ID, eModelType.TAG));
-        _db.execSQL(LogInfo.createTriggerDeletion(Tag.TABLE_NAME, Tag.COLUMN.ID, eModelType.TAG));
-        _db.execSQL(LogInfo.createTriggerUpdate(Tag.TABLE_NAME, Tag.COLUMN.ID, eModelType.TAG));
-
     }
 
     @Override
@@ -103,6 +56,52 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         if (currentVersion < 3 && 3 <= _newVersion) {
             // do upgrade to version 3
             // inroduction of triggers and logging db
+            _db.execSQL(LogInfo.DB_CREATE_V1);
+
+            // create trigger category
+            _db.execSQL(LogInfo.createTriggerInsertion(Category.TABLE_NAME, Category.COLUMN.ID, eModelType.CATEGORY));
+            _db.execSQL(LogInfo.createTriggerDeletion(Category.TABLE_NAME, Category.COLUMN.ID, eModelType.CATEGORY));
+            _db.execSQL(LogInfo.createTriggerUpdate(Category.TABLE_NAME, Category.COLUMN.ID, eModelType.CATEGORY));
+
+            // create trigger list
+            _db.execSQL(LogInfo.createTriggerInsertion(ShoppingList.TABLE_NAME, ShoppingList.COLUMN.ID, eModelType.LIST));
+            _db.execSQL(LogInfo.createTriggerDeletion(ShoppingList.TABLE_NAME, ShoppingList.COLUMN.ID, eModelType.LIST));
+            _db.execSQL(LogInfo.createTriggerUpdate(ShoppingList.TABLE_NAME, ShoppingList.COLUMN.ID, eModelType.LIST));
+
+            // create trigger product
+            _db.execSQL(LogInfo.createTriggerInsertion(Product.TABLE_NAME, Product.COLUMN.ID, eModelType.PRODUCT));
+            _db.execSQL(LogInfo.createTriggerDeletion(Product.TABLE_NAME, Product.COLUMN.ID, eModelType.PRODUCT));
+            _db.execSQL(LogInfo.createTriggerUpdate(Product.TABLE_NAME, Product.COLUMN.ID, eModelType.PRODUCT));
+
+            // create trigger taggedproduct
+            _db.execSQL(LogInfo.createTriggerInsertion(TaggedProduct.TABLE_NAME, TaggedProduct.COLUMN.ID, eModelType.TAGGED_PRODUCT));
+            _db.execSQL(LogInfo.createTriggerDeletion(TaggedProduct.TABLE_NAME, TaggedProduct.COLUMN.ID, eModelType.TAGGED_PRODUCT));
+            _db.execSQL(LogInfo.createTriggerUpdate(TaggedProduct.TABLE_NAME, TaggedProduct.COLUMN.ID, eModelType.TAGGED_PRODUCT));
+
+            // create trigger list entry
+            _db.execSQL(LogInfo.createTriggerInsertion(ListEntry.TABLE_NAME, ListEntry.COLUMN.ID, eModelType.LIST_ENTRY));
+            _db.execSQL(LogInfo.createTriggerDeletion(ListEntry.TABLE_NAME, ListEntry.COLUMN.ID, eModelType.LIST_ENTRY));
+            _db.execSQL(LogInfo.createTriggerUpdate(ListEntry.TABLE_NAME, ListEntry.COLUMN.ID, eModelType.LIST_ENTRY));
+
+            // create trigger ingredient
+            _db.execSQL(LogInfo.createTriggerInsertion(Ingredient.TABLE_NAME, Ingredient.COLUMN.ID, eModelType.INGREDIENT));
+            _db.execSQL(LogInfo.createTriggerDeletion(Ingredient.TABLE_NAME, Ingredient.COLUMN.ID, eModelType.INGREDIENT));
+            _db.execSQL(LogInfo.createTriggerUpdate(Ingredient.TABLE_NAME, Ingredient.COLUMN.ID, eModelType.INGREDIENT));
+
+            // create trigger recipe
+            _db.execSQL(LogInfo.createTriggerInsertion(Recipe.TABLE_NAME, Recipe.COLUMN.ID, eModelType.RECIPE));
+            _db.execSQL(LogInfo.createTriggerDeletion(Recipe.TABLE_NAME, Recipe.COLUMN.ID, eModelType.RECIPE));
+            _db.execSQL(LogInfo.createTriggerUpdate(Recipe.TABLE_NAME, Recipe.COLUMN.ID, eModelType.RECIPE));
+
+            // create trigger unit
+            _db.execSQL(LogInfo.createTriggerInsertion(Unit.TABLE_NAME, Unit.COLUMN.ID, eModelType.UNIT));
+            _db.execSQL(LogInfo.createTriggerDeletion(Unit.TABLE_NAME, Unit.COLUMN.ID, eModelType.UNIT));
+            _db.execSQL(LogInfo.createTriggerUpdate(Unit.TABLE_NAME, Unit.COLUMN.ID, eModelType.UNIT));
+
+            // create trigger unit
+            _db.execSQL(LogInfo.createTriggerInsertion(Tag.TABLE_NAME, Tag.COLUMN.ID, eModelType.TAG));
+            _db.execSQL(LogInfo.createTriggerDeletion(Tag.TABLE_NAME, Tag.COLUMN.ID, eModelType.TAG));
+            _db.execSQL(LogInfo.createTriggerUpdate(Tag.TABLE_NAME, Tag.COLUMN.ID, eModelType.TAG));
         }
     }
 }
